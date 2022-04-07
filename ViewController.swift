86354eebd776ef8c2b38e729f17c5e1d715907df
Tez <tez.park@sendbird.com>
2022-04-07 21:23:41 +0900

    test

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PodRenameTest
+//
+//  Created by Tez Park on 2022/04/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
